7a23e3f9fa91943f1f9387ca1be081521962596d
Jian Dong Guo <guojd@yahoo.com>
2022-02-02 15:34:47 -0500

    Added some XML comment features

--- PolicyManagementWeb/PolicyManagmentWebApi/Repositories/ISearchPolicyRepository.cs
+++ PolicyManagementWeb/PolicyManagmentWebApi/Repositories/ISearchPolicyRepository.cs
@@ -3,9 +3,23 @@ using System.Threading.Tasks;
 
 namespace PolicyManagementWebApi.Repositories
 {
+    /// <summary>
+    /// Add or get Search Policy from Database 
+    /// </summary>
     public interface ISearchPolicyRepository
     {
+        /// <summary>
+        /// Add search result command object into DB
+        /// </summary>
+        /// <param name="resultCommand">An search result command object<see cref="SearchPolicyResultCommand"/></param>
+        /// <returns></returns>
         Task AddSearchPolicyResultCommand(SearchPolicyResultCommand resultCommand);
+
+        /// <summary>
+        /// Get search policy result command object from DB
+        /// </summary>
+        /// <param name="searchId">The unique search Id</param>
+        /// <returns>A search policy result command object <see cref="ISearchPolicyResultCommand"></see></returns>
         Task<ISearchPolicyResultCommand> GetSearchPolicyResultCommand(string searchId);
     }
 }
--- PolicyManagementWeb/PolicyManagmentWebApi/Repositories/MongoSearchPolicyRepository.cs
+++ PolicyManagementWeb/PolicyManagmentWebApi/Repositories/MongoSearchPolicyRepository.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 
 namespace PolicyManagementWebApi.Repositories
 {
+    /// <summary>
+    /// Add or get Search Policy Result from MonogoDB
+    /// </summary>
     public class MongoSearchPolicyRepository : ISearchPolicyRepository
     {
         private readonly IMongoCollection<ISearchPolicyResultCommand> _searchPolicyResults;
@@ -19,6 +22,11 @@ namespace PolicyManagementWebApi.Repositories
             this._searchPolicyResults = database.GetCollection<ISearchPolicyResultCommand>(settings.SearchPolicyCollectionName);
         }
 
+        /// <summary>
+        /// Get search policy result command object from MongoDB
+        /// </summary>
+        /// <param name="searchId">The unique search Id</param>
+        /// <returns>A search policy result command object <see cref="ISearchPolicyResultCommand"></see></returns>
         public async Task<ISearchPolicyResultCommand> GetSearchPolicyResultCommand(string searchId)
         {
             List<ISearchPolicyResultCommand> resultCommands = await this._searchPolicyResults.Find(x => true).ToListAsync();
@@ -27,6 +35,11 @@ namespace PolicyManagementWebApi.Repositories
             return resultCommand;
         }
 
+        /// <summary>
+        /// Add search result command object into MongoDB
+        /// </summary>
+        /// <param name="resultCommand">An search result command object<see cref="SearchPolicyResultCommand"/></param>
+        /// <returns></returns>
         public async Task AddSearchPolicyResultCommand(SearchPolicyResultCommand resultCommand)
         {
             await this._searchPolicyResults.InsertOneAsync(resultCommand);
