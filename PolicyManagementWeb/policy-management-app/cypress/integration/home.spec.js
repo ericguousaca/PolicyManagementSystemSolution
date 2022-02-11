d00c41d14672c51254c635af0f04d49d5b986051
Jian Dong Guo <guojd@yahoo.com>
2022-02-11 12:27:18 -0500

    Update cypress test

--- PolicyManagementWeb/policy-management-app/cypress/integration/home.spec.js
+++ PolicyManagementWeb/policy-management-app/cypress/integration/home.spec.js
@@ -2,21 +2,23 @@ const { createYield } = require("typescript")
 
 describe('Home Page (list-all-policy)', () => {
   beforeEach(() => {
-
-    cy.fixture('policy-details.json').as("polictDetailsJSON");
-
-    cy.server();
-
-    cy.route('https://localhost:44330/api/v1.0/Policy/GetAll', "@polictDetailsJSON").as("mockPolicyDetails");
-
-    cy.visit('/');
-
+      //cy.intercept({ method: 'GET', url: '**/GetAll*', resultData }).as("mockPolicyDetailGridDataResult");
+      cy.viewport(1600, 720)
   });
 
   it('should list all policies', () => {
-    cy.get('.text-decoration-underline').contains('List All Policies');
-    cy.wait("@mockPolicyDetails");
-    cy.get('table > tbody > tr').should("have.length", 4);
-    cy.contains('table > tbody > tr:nth-child(4) > td:nth-child(6)', 'Test Company #4');
+    cy.fixture('policyDetailGridDataResult.json').then(resultData => {
+      console.log(resultData)
+      cy.server()
+      cy.intercept('GET', 'https://localhost:44330/api/v1.0/Policy/GetAll?skip=0&pageSize=5&sortBy=id&sortDirection=asc', resultData).as("mockPolicyDetailGridDataResult");
+
+      cy.visit('/');
+
+      cy.wait("@mockPolicyDetailGridDataResult");
+
+      cy.get('.text-decoration-underline').contains('List All Policies');
+      cy.get('table > tbody > tr').should("have.length", 5);
+      cy.contains('table > tbody > tr:nth-child(4) > td:nth-child(6)', 'Test Company #4');
+    });
   })
 })
